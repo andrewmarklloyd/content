df60acd316c63dfa8fb79fcb82be79bbf09f2ee1
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-01-12 17:26:41 -0800

    Add material angular and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+import { MatMenuModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
